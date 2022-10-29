c4ff73b97efc908f61d0e2a542858958bb5f6d6f
meliodas0001 <riquemorozinee@gmail.com>
2022-10-29 20:46:40 -0300

    Update

--- src/utils/SlashBuilder/index.ts
+++ src/utils/SlashBuilder/index.ts
@@ -46,7 +46,7 @@ const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
       }
     );
 
-    console.log(`Successfully reloaded application (/) commands.`);
+    console.log(`Successfully reloaded application (/) commands..`);
   } catch (error) {
     console.error(error);
   }
